90d0b6dc4f918eb8f048b3c92c4105b8f8e932e3
asdasd <jufranhundo@gmail.com>
2023-02-21 23:18:07 +0800

    asdasd

---
+++ ci4/app/Views/pages/myweb.php
@@ -0,0 +1,6 @@
+<header>
+		new Website
+	</header>
+	<div class="container">
+		<h1>Carl Justin Bustamante</h1>
+	</div>
---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Me</title>
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+			margin: 0;
+			padding: 0;
+			background-color: #f5f5f5;
+		}
+		header {
+			background-color: #333;
+			color: #fff;
+			padding: 20px;
+			text-align: center;
+			font-size: 30px;
+		}
+		.container {
+			max-width: 800px;
+			margin: 0 auto;
+			padding: 20px;
+			background-color: #fff;
+			box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+		}
+		h1 {
+			font-size: 36px;
+			margin-top: 0;
+		}
+		p {
+			font-size: 18px;
+			line-height: 1.5;
+		}
+	</style>
+</head>
+<body>
