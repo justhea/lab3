7d2f6bd7ea03b80e503b0ee0dc37db4d9e33702c
asdasd <jufranhundo@gmail.com>
2023-03-05 21:04:44 +0800

    Updated contetns

--- ci4/app/Views/pages/main.php
+++ ci4/app/Views/pages/main.php
@@ -33,7 +33,7 @@
 	<div class="container px-xl-0">
 		<div class="row justify-content-center">
 			<div class="col-xl-8 col-lg-10">
-				<h2 class="small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Artivise</h2>
+				<h2 class="small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Artivize</h2>
 				<div class="mt-35 mb-65 f-22 color-heading text-adaptive description" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
 				Artivize is a platform for artists to showcase their artworks online. It is a combination of "Art" and "Revitalize", which implies the revitalization of the art world through technology. 
 				</div>
@@ -45,21 +45,21 @@
 						<div class="color-heading text-adaptive">
 						Artivize provides artists with an online gallery to showcase their artworks to the internet. This feature allows artists to reach a wider audience and increase their chances of making sales.
 						</div>
-						<a href="#" class="mt-30 btn sm w-40 border-transparent-main"><i class="fas fa-angle-right"></i></a>
+						<a href="#main" class="mt-30 btn sm w-40 border-transparent-main"><i class="fas fa-angle-right"></i></a>
 					</div>
 					<div class="mb-50 col-md-4 col-sm-8" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">
 						<div class="mb-20 f-18 medium title">Curated Exhibitions</div>
 						<div class="color-heading text-adaptive">
 						Artivize curates online exhibitions of artworks from different artists, styles, and themes. This feature provides exposure and recognition to emerging artists, and it allows viewers to explore and appreciate different forms of art.
 						</div>
-						<a href="#" class="mt-30 btn sm w-40 border-transparent-main"><i class="fas fa-angle-right"></i></a>
+						<a href="#main" class="mt-30 btn sm w-40 border-transparent-main"><i class="fas fa-angle-right"></i></a>
 					</div>
 					<div class="mb-50 col-md-4 col-sm-8" data-aos-duration="600" data-aos="fade-down" data-aos-delay="1200">
 						<div class="mb-20 f-18 medium title">Open Submissions</div>
 						<div class="color-heading text-adaptive">
 						Artivize provides a platform for artists to showcase their work without censorship or judgment, promoting freedom of expression and creativity without limitations or restrictions.
 						</div>
-						<a href="#" class="mt-30 btn sm w-40 border-transparent-main"><i class="fas fa-angle-right"></i></a>
+						<a href="#main" class="mt-30 btn sm w-40 border-transparent-main"><i class="fas fa-angle-right"></i></a>
 					</div>
 				</div>
 			</div>
@@ -112,47 +112,15 @@ With the blessing of the website creator Carl, we are welcoming everyone to shar
 				</div>
 			</div>
 		</div>
+
+
+
+<!-- Main Content -->
 		<div class="row">
 			<div class="col-md-4 col-sm-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
-				<a href="#" class="mt-50 link color-main">
 					<img srcset="i/showcase_2_img_1@2x.jpg 2x" src="i/showcase_2_img_1.jpg" class="radius10 img-fluid" alt="" />
-					<div class="mt-20 mb-15 f-22 title">In the Sky</div>
-					<div class="color-heading f-14 semibold text-uppercase sp-20">Nicole Milton</div>
-				</a>
-			</div>
-			<div class="col-md-4 col-sm-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
-				<a href="#" class="mt-50 link color-main">
-					<img srcset="i/showcase_2_img_2@2x.jpg 2x" src="i/showcase_2_img_2.jpg" class="radius10 img-fluid" alt="" />
-					<div class="mt-20 mb-15 f-22 title">Grand Canyon</div>
-					<div class="color-heading f-14 semibold text-uppercase sp-20">Nicole Milton</div>
-				</a>
-			</div>
-			<div class="col-md-4 col-sm-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
-				<a href="#" class="mt-50 link color-main">
-					<img srcset="i/showcase_2_img_3@2x.jpg 2x" src="i/showcase_2_img_3.jpg" class="radius10 img-fluid" alt="" />
-					<div class="mt-20 mb-15 f-22 title">Man from the South</div>
-					<div class="color-heading f-14 semibold text-uppercase sp-20">Nicole Milton</div>
-				</a>
-			</div>
-			<div class="col-md-4 col-sm-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
-				<a href="#" class="mt-50 link color-main">
-					<img srcset="i/showcase_2_img_4@2x.jpg 2x" src="i/showcase_2_img_4.jpg" class="radius10 img-fluid" alt="" />
-					<div class="mt-20 mb-15 f-22 title">It's just a Cigarette</div>
-					<div class="color-heading f-14 semibold text-uppercase sp-20">Nicole Milton</div>
-				</a>
-			</div>
-			<div class="col-md-4 col-sm-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
-				<a href="#" class="mt-50 link color-main">
-					<img srcset="i/showcase_2_img_5@2x.jpg 2x" src="i/showcase_2_img_5.jpg" class="radius10 img-fluid" alt="" />
-					<div class="mt-20 mb-15 f-22 title">Mountains</div>
-					<div class="color-heading f-14 semibold text-uppercase sp-20">Nicole Milton</div>
-				</a>
-			</div>
-			<div class="col-md-4 col-sm-6" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
-				<a href="#" class="mt-50 link color-main">
-					<img srcset="i/showcase_2_img_6@2x.jpg 2x" src="i/showcase_2_img_6.jpg" class="radius10 img-fluid" alt="" />
-					<div class="mt-20 mb-15 f-22 title">Trip to Dream</div>
-					<div class="color-heading f-14 semibold text-uppercase sp-20">Nicole Milton</div>
+					<div class="mt-20 mb-15 f-22 title">Funny Dog</div>
+					<div class="color-heading f-14 semibold text-uppercase sp-20">Real</div>
 				</a>
 			</div>
 		</div>
@@ -161,79 +129,6 @@ With the blessing of the website creator Carl, we are welcoming everyone to shar
 
 
 
-
-<!-- TO be implemented
-
-
-<section class="pricing_table_2 bg-light pt-80 pb-70 text-center">
-	<div class="container px-xl-0">
-		<div class="f-14 semibold text-uppercase sp-20 color-heading" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Pricing Plan</div>
-		<h2 class="mt-15 mb-55 small" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">Clear &amp; Simple</h2>
-		<div class="row justify-content-center no-gutters">
-			<div class="col-xl-10">
-				<div class="row justify-content-center">
-					<div class="col-lg-3 col-md-5 col-sm-6 mb-70 mb-lg-0 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">
-						<div class="f-22 title">Free</div>
-						<div class="mt-10 color-heading subtitle">$0/month</div>
-						<div class="mx-auto mt-25 bg-gray f-62 lh-130 color-heading w-130 h-130 radius_full">
-							<i class="fas fa-bolt"></i> 
-						</div>
-						<a href="#" class="mt-40 btn border-gray color-main">Get Started</a>
-						<div class="mt-45 text-adaptive">
-							<div>Up to 5 Members</div>
-							<div>Backups for 2 Weeks</div>
-							<div>50 Mb per File</div>
-							<div>100 Messages</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-5 col-sm-6 mb-70 mb-lg-0 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
-						<div class="f-22 title">Starter</div>
-						<div class="mt-10 color-heading subtitle">$9.99/month</div>
-						<div class="mx-auto mt-25 bg-gray f-62 lh-130 color-heading w-130 h-130 radius_full">
-							<i class="fas fa-bomb"></i> 
-						</div>
-						<a href="#" class="mt-40 btn border-gray color-main">Get Started</a>
-						<div class="mt-45 text-adaptive">
-							<div>Up to 10 Members</div>
-							<div>Backups for 2 Month</div>
-							<div>250 Mb per File</div>
-							<div>1000 Messages</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-5 col-sm-6 mb-70 mb-sm-0 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
-						<div class="f-22 title">Standart</div>
-						<div class="mt-10 color-heading subtitle">$19.99/month</div>
-						<div class="mx-auto mt-25 bg-gray f-62 lh-130 color-heading w-130 h-130 radius_full">
-							<i class="fas fa-heart"></i> 
-						</div>
-						<a href="#" class="mt-40 btn action-1">Get Started</a>
-						<div class="mt-45 text-adaptive">
-							<div>Up to 20 Members</div>
-							<div>Backups for 6 Month</div>
-							<div>Unlimited Size per File</div>
-							<div>Unlimited Messages</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-md-5 col-sm-6 block" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">
-						<div class="f-22 title">Enterprise</div>
-						<div class="mt-10 color-heading subtitle">$49.99/month</div>
-						<div class="mx-auto mt-25 bg-gray f-62 lh-130 color-heading w-130 h-130 radius_full">
-							<i class="fas fa-building"></i> 
-						</div>
-						<a href="#" class="mt-40 btn border-gray color-main">Get Started</a>
-						<div class="mt-45 text-adaptive">
-							<div>Unlimited Members</div>
-							<div>Backups for 12 Month</div>
-							<div>Unlimited Size per File</div>
-							<div>Unlimited Messages</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>	
--->
 
 
 
@@ -269,34 +164,6 @@ With the blessing of the website creator Carl, we are welcoming everyone to shar
 	</div>
 </section>
 
-<!-- Form 1 -->
-
-<section class="form_1 pt-120 pb-120">
-	<div class="container px-xl-0">
-		<form action="form-handler.php" method="post" class="bg-light mx-auto mw-430 radius10 pt-40 px-50 pb-30">
-			<h2 class="mb-40 small text-center" data-aos-duration="600" data-aos="fade-down" data-aos-delay="0">Sign Up Now</h2>
-			<div class="mb-20 input_holder" data-aos-duration="600" data-aos="fade-down" data-aos-delay="150">
-				<input type="email" name="email" placeholder="Your email" class="input border-gray focus-action-1 color-heading placeholder-heading w-full" />
-			</div>
-			<div class="mb-20 input_holder" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
-				<input type="password" name="password" placeholder="Your password" class="input border-gray focus-action-1 color-heading placeholder-heading w-full" />
-			</div>
-			<div data-aos-duration="600" data-aos="fade-down" data-aos-delay="450">
-				<input type="checkbox" name="rules" class="d-none border-gray focus-action-1" id="form_1_checkbox" checked />
-				<label for="form_1_checkbox" class="mt-25 color-heading w-full">
-					I agree to the Terms of Service
-				</label>
-			</div>
-			<div data-aos-duration="600" data-aos="fade-down" data-aos-delay="600">
-				<button class="mt-25 btn action-1 w-full">Create an Account</button>
-			</div>
-			<div class="mt-50 hr bg-gray h-1" data-aos-duration="600" data-aos="fade-down" data-aos-delay="750"></div>
-			<div class="mt-25 f-18 medium color-heading text-center" data-aos-duration="600" data-aos="fade-down" data-aos-delay="900">
-				Do you have an Account? <a href="#" class="link action-1">Sign In</a>
-			</div>
-		</form>
-	</div>
-</section>
 <!-- Footer 1 -->
 
 <footer class="footer_1 bg-light pt-75 pb-65 text-center">
@@ -310,8 +177,8 @@ With the blessing of the website creator Carl, we are welcoming everyone to shar
 			</div>
 			<div class="col-lg-4 col-sm-6 text-sm-left text-lg-right order-2 order-lg-0" data-aos-duration="600" data-aos="fade-down" data-aos-delay="300">
 
-				<a href="#" class="mx-15 link color-main"><i class="fab fa-twitter"></i></a>
-				<a href="#" class="mx-15 link color-main"><i class="fab fa-facebook-square"></i></a>
+				<a href="https://www.twitter.com/dyastin_223" class="mx-15 link color-main"><i class="fab fa-twitter"></i></a>
+				<a href="https://www.facebook.com/justheaa" class="mx-15 link color-main"><i class="fab fa-facebook-square"></i></a>
 				<a href="#" class="ml-15 link color-main"><i class="fab fa-google-plus-g"></i></a>
 			</div>
 		</div>
@@ -324,26 +191,4 @@ With the blessing of the website creator Carl, we are welcoming everyone to shar
 		</div>
 	</div>
 </footer>
-<!-- forms alerts -->
-<div class="alert alert-success alert-dismissible alert-form-success" role="alert">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	Thanks for your message!
-</div>
-<div class="alert alert-warning alert-dismissible alert-form-check-fields" role="alert">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	Please, fill in required fields.
-</div>
-<div class="alert alert-danger alert-dismissible alert-form-error" role="alert">
-	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	An error occurred while sending data :(
-</div>
-<!-- popup with video -->
-<div class="overlay"></div>
-
-<div class="video_popup">
-	<a class="close">
-		<img srcset="i/close_white@2x.png 2x" src="i/close_white.png" alt="" />
-	</a>
-	<div class="d-flex align-items-center justify-content-center w-full h-full iframe_container"></div>
-</div>
 
